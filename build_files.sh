2a8c0b0b9e956867c8e3d19d56e4f4d2659efb53
dimejiwebtech <dimejiweb@gmail.com>
2026-03-30 07:09:33 +0100

    Fix Vercel build failure (PEP 668)

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
 echo "Collecting static files..."
 python3.12 manage.py collectstatic --noinput --clear
