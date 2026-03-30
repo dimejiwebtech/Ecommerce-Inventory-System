8e2c51518109a9dd201c3f3f7902ffba00147298
dimejiwebtech <dimejiweb@gmail.com>
2026-03-30 07:34:11 +0100

    Migrate to Modern Vercel Functions

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build the project
-echo "Building the project..."
-python3.12 -m pip install -r requirements.txt --break-system-packages
-
-echo "Collecting static files..."
-python3.12 manage.py collectstatic --noinput --clear
